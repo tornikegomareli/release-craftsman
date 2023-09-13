04507b99a70da02ac38131e1f863ca03711bf433
tornikegomareli <gomarelidevelopment@gmail.com>
2023-09-14 02:25:29 +0400

    Moved inital logics into small functions

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use git_log_format::GitLogFormat;
 
 use clap::{App, Arg};
 use std::env;
+use std::io::Result;
 use std::process::Command;
 use std::str;
 
-fn main() {
+fn parse_args() -> GitLogFormat {
     let matches = App::new("ReleaseCraftman")
         .version("1.0")
         .arg(
@@ -18,70 +19,59 @@ fn main() {
         )
         .get_matches();
 
-    let log_format = match matches.value_of("format").unwrap_or("compact") {
+    match matches.value_of("format").unwrap_or("compact") {
         "compact" => GitLogFormat::Compact,
         "hard" => GitLogFormat::Full,
         _ => {
-            eprintln!("Invalid format specified. Use 'compact' or 'hard'.");
-            return;
+            eprintln!("Invalid format specified. Using 'compact' as default.");
+            GitLogFormat::Compact
         }
+    }
+}
+
+fn get_repo_path() -> Result<String> {
+    let repo_path = if cfg!(debug_assertions) {
+        println!("Running in debug mode");
+        String::from("/Users/tornike-mac/Development/Composable2048")
+    } else {
+        println!("Running in release mode");
+        let current_dir = env::current_dir()?;
+        current_dir.to_str().unwrap().to_string()
     };
 
-    let repo_path: String;
+    Ok(repo_path)
+}
 
-    #[cfg(debug_assertions)]
-    {
-        println!("Running in debug mode");
-        // Testing purpose
-        repo_path = String::from("/Users/tornike-mac/Development/Composable2048");
-    }
-
-    #[cfg(not(debug_assertions))]
-    {
-        println!("Running in release mode");
-        let current_dir = match env::current_dir() {
-            Ok(dir) => dir,
-            Err(e) => {
-                eprintln!("Error fetching current directory: {}", e);
-                return;
-            }
-        };
-
-        repo_path = match current_dir.to_str() {
-            Some(path) => String::from(path),
-            None => {
-                eprintln!("Error converting path to string");
-                return;
-            }
-        };
-    }
-
-    println!("Executing git log command in directory: {}", repo_path);
-    let output = match Command::new("git")
+fn execute_git_log(format: &GitLogFormat, repo_path: &String) -> Result<String> {
+    let output = Command::new("git")
         .arg("log")
-        .arg(log_format.to_git_arg())
+        .arg(format.to_git_arg())
         .arg("-n")
         .arg("5")
-        .current_dir(&repo_path)
-        .output()
-    {
-        Ok(output) => output,
-        Err(e) => {
-            eprintln!("Failed to execute git command: {}", e);
-            return;
-        }
-    };
+        .current_dir(repo_path)
+        .output()?;
 
-    let output_str = match str::from_utf8(&output.stdout) {
-        Ok(str) => str,
-        Err(e) => {
-            eprintln!("Could not convert to UTF-8: {}", e);
-            return;
-        }
-    };
+    let output_str = String::from_utf8(output.stdout)
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?;
+    Ok(output_str)
+}
 
-    let commits: Vec<&str> = output_str.lines().collect();
-    for (_i, commit) in commits.iter().enumerate() {
-        println!("{}", commit);
+fn print_commits(commits: Vec<&str>) {
+    for (i, commit) in commits.iter().enumerate() {
+        println!("Commit {}: {}", i + 1, commit);
     }
+}
+
+fn main() -> Result<()> {
+    let log_format = parse_args();
+    let repo_path = get_repo_path()?;
+
+    println!("Executing git log command in directory: {}", repo_path);
+
+    let output_str = execute_git_log(&log_format, &repo_path)?;
+    let commits: Vec<&str> = output_str.lines().collect();
+
+    print_commits(commits);
+
+    Ok(())
 }
