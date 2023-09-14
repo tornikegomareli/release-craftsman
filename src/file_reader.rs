14122422b2fe12a280144b480f38d894fc44b6b0
tornikegomareli <gomarelidevelopment@gmail.com>
2023-09-15 01:20:09 +0400

    chat gpt, custom prompts

--- src/file_reader.rs
+++ src/file_reader.rs
@@ -1,6 +1,10 @@
+use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
+use include_dir::{include_dir, Dir};
+
+const PROMPTS_DIR: Dir = include_dir!("src/prompts");
 
 pub enum PromptType {
     GeneralMarkdown,
@@ -25,40 +29,21 @@ impl FileReader {
         FileReader {}
     }
 
-    /// Reads a prompt file and replaces placeholders with actual data.
-    ///
-    /// # Arguments
-    ///
-    /// * `prompt_type` - An enum representing the type of prompt
-    /// * `version` - A string slice containing the version to insert into the prompt
-    /// * `commit_logs` - A string slice containing the commit logs to insert into the prompt
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// let fr = FileReader::new();
-    /// let prompt = fr.read_and_replace(PromptType::GeneralMarkdown, "v1.2.3", "My commit logs");
-    /// ```
     pub fn read_and_replace(
         &self,
         prompt_type: PromptType,
         version: &str,
         commit_logs: &str,
     ) -> io::Result<String> {
-        // Open file
-        let path = Path::new(prompt_type.file_name());
-        let file = File::open(&path)?;
+        let file = PROMPTS_DIR
+            .get_file(prompt_type.file_name())
+            .ok_or(io::Error::new(io::ErrorKind::NotFound, "File not found"))?;
 
-        // Read file into a string
-        let reader = BufReader::new(file);
-        let mut content = String::new();
-        for line in reader.lines() {
-            content.push_str(&line?);
-            content.push('\n');
-        }
+        let content_str = std::str::from_utf8(file.contents())
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
 
         // Replace placeholders
-        let content_replaced_version = content.replace("&VERSION&", version);
+        let content_replaced_version = content_str.replace("&VERSION&", version);
         let content_replaced_all = content_replaced_version.replace("&COMMIT_LOGS&", commit_logs);
 
         Ok(content_replaced_all)
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 mod git_log_format;
 use git_log_format::GitLogFormat;
 
+mod file_reader;
+use file_reader::FileReader;
+use file_reader::PromptType;
+
 use clap::{App, Arg};
 use std::env;
 use std::io::Result;
 use std::process::Command;
 use std::str;
+use include_dir::{include_dir, Dir};
 
-fn parse_args() -> (GitLogFormat, Option<String>, Option<String>) {
+use chat_gpt_lib_rs::{ChatGPTClient, ChatInput, Message, Model, Role};
+
+fn parse_args() -> (GitLogFormat, Option<String>, Option<String>, String, Model, Option<String>) {
     let matches = App::new("ReleaseCraftman")
         .version("1.0")
         .arg(
@@ -31,6 +38,29 @@ fn parse_args() -> (GitLogFormat, Option<String>, Option<String>) {
                 .takes_value(true)
                 .help("Ending tag for the git log"),
         )
+        .arg(
+            Arg::with_name("api_key")
+                .short("k")
+                .long("key")
+                .takes_value(true)
+                .required(true)
+                .help("API Key for GPT"),
+        )
+        .arg(
+            Arg::with_name("model")
+                .short("m")
+                .long("model")
+                .takes_value(true)
+                .default_value("Gpt_3_5Turbo")
+                .help("GPT model to use (Gpt_3_5Turbo|Gpt_4)"),
+        )
+        .arg(
+            Arg::with_name("version")
+                .short("v")
+                .long("version")
+                .takes_value(true)
+                .help("Version of the release")
+        )
         .get_matches();
 
     let log_format = match matches.value_of("format").unwrap_or("compact") {
@@ -42,7 +72,16 @@ fn parse_args() -> (GitLogFormat, Option<String>, Option<String>) {
     let start_tag = matches.value_of("start_tag").map(|s| s.to_string());
     let end_tag = matches.value_of("end_tag").map(|e| e.to_string());
 
-    (log_format, start_tag, end_tag)
+    let api_key = matches.value_of("api_key").unwrap().to_string();
+    let model = match matches.value_of("model").unwrap() {
+        "Gpt_4" => Model::Gpt_4,
+        "Gpt_3_5Turbo" => Model::Gpt3_5Turbo,
+        _ => Model::Gpt3_5Turbo,
+    };
+
+    let version = matches.value_of("version").map(|v| v.to_string());
+
+    (log_format, start_tag, end_tag, api_key, model, version)
 }
 
 fn get_repo_path() -> Result<String> {
@@ -89,16 +128,63 @@ fn print_commits(commits: Vec<&str>) {
     }
 }
 
-fn main() -> Result<()> {
-    let (log_format, start_tag, end_tag) = parse_args();
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Parsing command line arguments and getting repo path
+    let (log_format, start_tag, end_tag, api_key, model, version) = parse_args();
     let repo_path = get_repo_path()?;
 
     println!("Executing git log command in directory: {}", repo_path);
 
+    // Fetching git logs
     let output_str = execute_git_log(&log_format, &repo_path, &start_tag, &end_tag)?;
     let commits: Vec<&str> = output_str.lines().collect();
 
-    print_commits(commits);
+    // Print commit logs (For debugging)
+    print_commits(commits.clone());
+
+    let fr = FileReader::new();
+    let version_str = version.unwrap_or_else(|| "1.0.0".to_string()); // Replace with your real version if available
+
+
+    // Convert commit logs Vec<&str> to a single String
+    let commit_logs = commits.join("\n");
+
+    // Choose a prompt type based on some criteria (here we're using GeneralMarkdown for demonstration)
+    // dynamically select this based on user input or other conditions
+    let prompt_type = PromptType::GeneralMarkdown;
+
+    // Generate the final prompt string
+    let final_prompt = fr.read_and_replace(prompt_type, &version_str, &commit_logs)?;
+
+    // Output the final prompt
+    println!("Final Prompt: {}", final_prompt);
+
+    let base_url = "https://api.openai.com";
+    let client = ChatGPTClient::new(&api_key, base_url);
+
+
+    let gpt_response = run_chat_gpt(&api_key, model, &final_prompt).await?;
+    println!("GPT response {}", gpt_response);
 
     Ok(())
+}
+
+async fn run_chat_gpt(api_key: &str, model: Model, message: &str) -> Result<String> {
+    let base_url = "https://api.openai.com";
+    let client = ChatGPTClient::new(api_key, base_url);
+
+    let chat_input = ChatInput { model, messages: vec![
+            Message {
+                role: Role::System,
+                content: "You are a helpful assistant.".to_string(),
+            },
+            Message {
+                role: Role::User,
+                content: message.to_string(),
+            },
+        ], ..Default::default() };
+
+    let response = client.chat(chat_input).await.unwrap();
+    Ok(response.choices[0].message.content.clone())
 }
